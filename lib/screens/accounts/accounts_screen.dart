8d7f3e13a976d9c5b1c02a093dfa7ae7ea5ec7d4
Prince <prince.mst13@itbhu.ac.in>
2023-09-08 22:25:58 +0530

    Update accounts_screen.dart

--- lib/screens/accounts/accounts_screen.dart
+++ lib/screens/accounts/accounts_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/screens/accounts/accounts_bloc.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:go_router/go_router.dart';
 
 class RootAccountsScreen extends StatefulWidget {
   const RootAccountsScreen({Key? key}) : super(key: key);
